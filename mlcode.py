e47b19aaf991da76dd39b7467f20f9e7a92f8e4e
disha1822 <disha108a@gmail.com>
2020-05-27 11:27:03 +0530

    updated

--- mlcode.py
+++ mlcode.py
@@ -3,58 +3,87 @@
 
 import sys
 import random
-import pandas as pd
 from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
 from keras.layers import Dense
-from keras.optimizers import Adam
-import seaborn as sns
-sns.set()
+from keras_preprocessing.image import ImageDataGenerator
+
 
 i = sys.argv[1]
-n = random.randint(3,7)
-
-dataset = pd.read_csv('/MLOPs/wines.csv')
-
+n =  random.randint(1,3)
+m =  random.randint(1,3)
 
 
-y = dataset['Class']
-
-
-print(y.value_counts())
-
-
-sns.scatterplot(x= dataset['Alcohol'], y = y)
-
-
-y = pd.get_dummies(y)            #### We can not drop one category of y as it is our Predicted
-
-print(y.head())
-
-X = dataset.drop('Class', axis = 1)
-
-print(X.info())
 
 model = Sequential()
 
-model.add(Dense(units = 12, input_shape = (13,), activation = 'relu', kernel_initializer = 'he_normal'))
-model.add(Dense(units = 16, activation = 'relu'))
-model.add(Dense(units = 8, activation = 'relu'))
-model.add(Dense(units = 12, activation = 'relu'))
+model.add(Convolution2D(filters=32, 
+                        kernel_size=(3,3), 
+                        activation='relu',
+                        input_shape=(64, 64, 3)
+                       ))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Convolution2D(filters=32, 
+                        kernel_size=(3,3), 
+                        activation='relu',
+                       ))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 if i==1:
     while n>0 :
-        model.add(Dense(units = random.randint(8,15), activation = 'relu'))
+        model.add(Convolution2D(filters=32, 
+                        kernel_size=(3,3), 
+                        activation='relu',
+                       ))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
         n=n-1
-  
-##### Output Layer ##########
-model.add(Dense(units = 3, activation = 'softmax'))
+
+
+model.add(Flatten())
+model.add(Dense(units=128, activation='relu'))
+model.add(Dense(units=256, activation='relu'))
+if i==1:
+    while m>0 :
+        model.add(Dense(units = random.choice([128,256,512]), activation = 'relu'))
+        m=m-1
+
+########## Output Layer ##########
+model.add(Dense(units=1, activation='sigmoid'))
 
 print(model.summary())
 
-model.compile(optimizer = Adam(learning_rate = 0.001), loss = 'categorical_crossentropy', metrics = ['accuracy'])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-model.fit(X,y, epochs = 50)
 
-accuracy = int(model.history.history['accuracy'][-1]*100)
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+test_datagen = ImageDataGenerator(rescale=1./255)
+training_set = train_datagen.flow_from_directory(
+        '/MLOPs/training_set/',
+        target_size=(64, 64),
+        batch_size=32,
+        class_mode='binary')
+test_set = test_datagen.flow_from_directory(
+        '/MLOPs/test_set/',
+        target_size=(64, 64),
+        batch_size=32,
+        class_mode='binary')
+model.fit(
+        training_set,
+        steps_per_epoch=4000,
+        epochs=25,
+        validation_data=test_set,
+        validation_steps=800)
+
+accuracy = int(model.history.history['val_accuracy'][-1]*100)
 
 print("final accuracy ",accuracy)
+
+
+
+
 
